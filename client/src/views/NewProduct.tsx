4c55e7e41c896bf7185c59a96ff8ca9aa63e7587
jhansarmiento <jhancsn1@gmail.com>
2025-01-31 23:38:02 -0500

    Routing entre las diferentes paginas

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom'
 
 export default function NewProduct() {
   return (
-    <div>New Product</div>
+    <>
+    <div className='flex justify-between'>
+      <h2 className='text-4xl font-black text-slate-500'>New Product</h2>
+      <Link 
+        to='/'
+        className='rounded-xl bg-indigo-600 text-sm p-3 text-white shadow hover:bg-indigo-500'>
+        Products
+      </Link>
+    </div>
+  </>
   )
 }
--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom'
 
 export default function Products() {
   return (
-    <div>Products</div>
+    <>
+      <div className='flex justify-between'>
+        <h2 className='text-4xl font-black text-slate-500'>Products</h2>
+        <Link 
+          to='products/new'
+          className='rounded-xl bg-indigo-600 text-sm p-3 text-white shadow hover:bg-indigo-500'>
+          New Product
+        </Link>
+      </div>
+    </>
   )
 }
