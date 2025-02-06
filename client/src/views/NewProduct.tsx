569b989e7c0475e0b9c014c9a5ee321622939642
jhansarmiento <jhancsn1@gmail.com>
2025-02-06 00:15:24 -0500

    Corrige los mensajes de errores en consola al agregar un producto

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,4 +1,4 @@
-import { Link, Form, useActionData, ActionFunctionArgs } from 'react-router-dom'
+import { Link, Form, useActionData, ActionFunctionArgs, redirect } from 'react-router-dom'
 import ErrorMessage from '../components/ErrorMessage'
 import { addProduct } from '../services/ProductService'
 
@@ -15,8 +15,9 @@ export async function action({request} : ActionFunctionArgs) {
     return error
   }
 
-  addProduct(data)
-  return null
+  await addProduct(data)
+
+  return redirect('/')
 }
 
 export default function NewProduct() {
